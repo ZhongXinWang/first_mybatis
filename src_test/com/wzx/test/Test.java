be757bb4abb0be86cfefbd1dd5e15312e27e4a27
Zhong Xin Wang <940945444@qq.com>
2017-05-03 17:16:29 +0800

    可以把结果输出到控制台

---
+++ src_test/com/wzx/test/Test.java
@@ -0,0 +1,40 @@
+package com.wzx.test;
+
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.wzx.mybatis.model.User;
+
+public class Test {
+    private static SqlSessionFactory sqlSessionFactory;
+    private static Reader reader; 
+    static{
+        try{
+        
+            reader    = Resources.getResourceAsReader("Configuration.xml");
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static SqlSessionFactory getSession(){
+        return sqlSessionFactory;
+    }
+    
+    public static void main(String[] args) {
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+        
+        User user = (User) session.selectOne("com.wzx.mybatis.model.User.selectUserById", 1);
+        System.out.println(user.getName());
+        System.out.println(user.getId());
+        } finally {
+        session.close();
+        }
+    }
+}
